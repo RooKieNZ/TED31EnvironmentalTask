8e61fa6c761abc0f86b040e6e6f256f3a6530be6
RooKie <40522084+RooKieNZ@users.noreply.github.com>
2019-04-28 08:09:14 +1200

    V0.5
    added new sprites and overall functionality

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
-#Brad_Toase AS91907GameProto V0.1
+#Brad_Toase AS91907GameProto V0.5?
+
 import sys
 import random
 from PIL import Image, ImageTk
@@ -6,11 +7,11 @@ from tkinter import Tk, Frame, Canvas, ALL, NW
 
 
 class Cons:
-    ###Constraints, overall sizing and different adjustable operands, may be used for future settings menu?###    
-    BOARD_WIDTH = 300
-    BOARD_HEIGHT = 300
+        
+    BOARD_WIDTH = 3000
+    BOARD_HEIGHT = 3000
     DELAY = 100
-    DOT_SIZE = 10
+    DOT_SIZE = 1
     MAX_RAND_POS = 27
 
 
@@ -27,13 +28,25 @@ class Board(Canvas):
         ###initializes game###
 
         self.inGame = True
-        self.dots = 10
+        self.dots = 3
+        self.score = 0
+        
+        # variables used to move snake object
+        self.moveX = Cons.DOT_SIZE
+        self.moveY = 0
         
         # starting bottle coordinates
         self.bottleX = 100
         self.bottleY = 190
         
         self.load_images()
+
+        #self.focus_get()
+
+        self.create_objects()
+        self.locate_bottle()
+        self.bind_all("<Key>", self.on_key_pressed)
+        self.after(Cons.DELAY, self.on_timer)
 
     def load_images(self):
         ###loads images from file###
@@ -53,11 +66,122 @@ class Board(Canvas):
         
     def create_objects(self):
         ###creates objects on Canvas###
+    
+        self.create_text(30, 10, text="Score: {0}".format(self.score), 
+                         tag="score", fill="white")
         self.create_image(self.bottleX, self.bottleY, image=self.bottle,
                           anchor=NW, tag="bottle")
         self.create_image(50, 50, image=self.head, anchor=NW,  tag="head")
-        self.create_image(30, 50, image=self.dot, anchor=NW, tag="dot")
-        self.create_image(40, 50, image=self.dot, anchor=NW, tag="dot")
+        self.create_image(20, 50, image=self.dot, anchor=NW, tag="dot")
+        self.create_image(0, 50, image=self.dot, anchor=NW, tag="dot")
+   
+    def check_bottle_collision(self):
+        ###checks if the head of snake collides with bottle###
+
+        bottle = self.find_withtag("bottle")
+        head = self.find_withtag("head")
+        
+        # print(self.bbox(head))
+        
+        x1, y1, x2, y2 = self.bbox(head)
+        overlap = self.find_overlapping(x1, y1, x2, y2)
+            
+        for ovr in overlap:
+          
+            if bottle[0] == ovr:
+                
+                self.score += 1
+                x, y = self.coords(bottle)
+                self.create_image(x, y, image=self.dot, anchor=NW, tag="dot")
+                self.locate_bottle()
+
+    def move_snake(self):
+        ###moves the Snake object###
+      
+        dots = self.find_withtag("dot")
+        head = self.find_withtag("head")
+                
+        items = dots + head
+        
+        z = 0
+        while z < len(items)-1:
+            
+            c1 = self.coords(items[z])
+            c2 = self.coords(items[z+1])
+            self.move(items[z], c2[0]-c1[0], c2[1]-c1[1])
+            z += 1
+            
+        self.move(head, self.moveX, self.moveY)     
+
+
+    def locate_bottle(self):
+        ###places the bottle object on Canvas###
+    
+        bottle = self.find_withtag("bottle")
+        self.delete(bottle[0])
+    
+        r = random.randint(0, Cons.MAX_RAND_POS)
+        self.bottleX = r * Cons.DOT_SIZE
+        r = random.randint(0, Cons.MAX_RAND_POS)
+        self.bottleY = r * Cons.DOT_SIZE
+        
+        self.create_image(self.bottleX, self.bottleY, anchor=NW,
+            image=self.bottle, tag="bottle")
+
+    def on_key_pressed(self, e): 
+        ###controls direction variables with cursor keys###
+    
+        key = e.keysym
+
+        LEFT_CURSOR_KEY = "Left"
+        if key == LEFT_CURSOR_KEY and self.moveX <= 0:
+            
+            self.moveX = -Cons.DOT_SIZE
+            self.moveY = 0
+        
+        RIGHT_CURSOR_KEY = "Right"
+        if key == RIGHT_CURSOR_KEY and self.moveX >= 0:
+            
+            self.moveX = Cons.DOT_SIZE
+            self.moveY = 0
+
+        UP_CURSOR_KEY = "Up"
+        if key == UP_CURSOR_KEY and self.moveY <= 0:
+                        
+            self.moveX = 0
+            self.moveY = -Cons.DOT_SIZE
+
+        DOWN_CURSOR_KEY = "Down"
+        if key == DOWN_CURSOR_KEY and self.moveY >= 0:
+            
+            self.moveX = 0
+            self.moveY = Cons.DOT_SIZE
+
+    def on_timer(self):
+        ###creates a game cycle each timer event###
+
+        self.draw_score()
+        self.check_collisions()
+
+        if self.inGame:
+            self.check_bottle_collision()
+            self.move_snake()
+            self.after(Cons.DELAY, self.on_timer)
+        else:
+            self.game_over()            
+
+    def draw_score(self):
+        ###draws score###
+        
+        score = self.find_withtag("score")
+        self.itemconfigure(score, text="Score: {0}".format(self.score))
+
+    def game_over(self):
+        ###deletes all objects and draws game over message###
+
+        self.delete(ALL)
+        self.create_text(self.winfo_width() /2, self.winfo_height()/2,
+                         text="Game Over with score {0}".format(self.score), fill="white")
 
 
 class Snake(Frame):
@@ -75,6 +199,11 @@ def main():
     root = Tk()
     Snake()
     root.mainloop()  
+
+
+if __name__ == '__main__':
+    main()
+
 
 
 if __name__ == '__main__':
